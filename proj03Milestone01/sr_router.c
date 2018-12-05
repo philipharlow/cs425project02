8235faad81b778fd9bcb442d186707c951c04b2f
marcossamayoa <35188076+marcossamayoa@users.noreply.github.com>
2018-12-05 07:06:56 -0700

    Add files via upload

--- proj03Milestone01/sr_router.c
+++ proj03Milestone01/sr_router.c
@@ -24,6 +24,7 @@
 #include "sr_protocol.h"
 
 void handleArp(struct sr_instance*, uint8_t*, unsigned int, char* );
+void handleForward(struct sr_instance* sr, uint8_t* packet, unsigned int len, char* interface );
 void sendReply(struct sr_instance*, uint8_t*, unsigned int, char*, struct sr_if* );
 void makeArp(
         struct sr_arphdr* arpHdr,
@@ -50,6 +51,12 @@ void makeEthernet(
         uint16_t type,
         uint8_t* src,
         uint8_t* dst );
+void arpSendRequest(struct sr_instance* sr,
+		struct sr_if* interfacePTR, 
+		uint32_t targetIP);
+int isBroadcast(struct sr_ethernet_hdr* eHdr);
+int packetForUs(struct sr_instance* sr, uint8_t* packet, const char* iface);
+
 
 #define ARP_CACHE_SIZE 100
 #define ARP_STALE_TIME 15   // in seconds
@@ -116,13 +123,26 @@ void sr_handlepacket(struct sr_instance* sr,
 
     struct sr_ethernet_hdr * ethernetHdr = (struct sr_ethernet_hdr *)packet;
 
-    if (ntohs(ethernetHdr->ether_type) == ETHERTYPE_IP) {
-        //to do for future milestone
-    }
-    //if our packet is an ARP
-    if (ntohs(ethernetHdr->ether_type) == ETHERTYPE_ARP) {
-        handleArp(sr, packet, len, interface);
-    }
+	// If this packet is a broadcast packet
+	if(isBroadcast(ethernetHdr){
+		handleArp(sr, packet, len, interface);
+	}
+	// If the packet is meant for us
+	else if(packetForUs(sr, packet, interface)){
+		
+		//if our packet is an IP
+		if (ntohs(ethernetHdr->ether_type) == ETHERTYPE_IP) {
+			handleIP(sr, packet,len, interface);
+		}
+		//if our packet is an ARP
+		if (ntohs(ethernetHdr->ether_type) == ETHERTYPE_ARP) {
+			handleArp(sr, packet, len, interface);
+		}
+	}
+	// The packet is not for us, so we must pass it along
+	else{
+		forwardPacket(sr, packet, len, interface);
+	}
 
 }/* end sr_ForwardPacket */
 
@@ -177,6 +197,64 @@ void handleArp( struct sr_instance* sr, uint8_t* packet, unsigned int len, char*
 
         // cache the new arp entry
         arpEntry(arpHeader);
+    }
+}
+
+/*--------------------------------------------------------------------- 
+ * Method: void handleForward(struct sr_instance*, uint8_t*,
+ *                            unsigned int, char* )
+ *
+ * This will handle what to do when we have to forward a packet
+ *---------------------------------------------------------------------*/
+void handleForward(struct sr_instance* sr, uint8_t* packet, unsigned int len, char* interface ){
+	
+    struct ip* ipHeader = (struct ip*)(packet+14);
+    struct sr_rt* rtptr = sr->routing_table;
+    int cachedEntry;
+
+    // loop through rtable for the next hop with the packet's destination ip
+    while (rtptr) {
+        if (rtptr->dest.s_addr == ipHeader->ip_dst.s_addr)
+            break;
+        else
+            rtptr = rtptr->next;
+    }
+
+    // if not in our rtable, send it out eth0
+    if (!rtptr) {
+        rtptr = sr->routing_table;          // eth0
+        if ((cachedEntry = arpSearchCache(rtptr->gw.s_addr)) > -1) {
+            forwardPacket(sr, packet, len, rtptr->interface, arpReturnEntryMac(cachedEntry));
+        } else {
+            cachePacket(sr, packet, len, rtptr);
+        }
+    }
+    
+    /* Check ARP cache for matching MAC, forward the packet. 
+     * otherwise, cache the packet and wait for an ARP reply
+     * to tell us the correct MAC address to use */
+    if ((cachedEntry = arpSearchCache(rtptr->gw.s_addr)) > -1) {
+        forwardPacket(sr, packet, len, rtptr->interface, arpReturnEntryMac(cachedEntry));       
+    } else {
+		struct ip* ipHeader = (struct ip*)(packet+14);
+		int i;
+
+		// request ARP for the unknown packet
+		arpSendRequest(sr, sr_get_interface(sr, rtptr->interface), rtptr->gw.s_addr);
+
+		// look through packet cache for the first empty entry
+		for (i = 0; i < PACKET_CACHE_SIZE; i++) {
+			if (packetCacheEntries[i].len == 0) 
+				break;
+		}
+
+		// copy packet data to cache
+		memcpy(&packetCacheEntries[i].packet, packet, len);
+		packetCacheEntries[i].nexthop = rtptr;
+		packetCacheEntries[i].tip = ipHeader->ip_dst.s_addr;
+		packetCacheEntries[i].len = len;
+		packetCacheEntries[i].arps = 1;
+		packetCacheEntries[i].timeCached = time(NULL);
     }
 }
 
@@ -290,6 +368,45 @@ void makeArp(
 }
 
 /*-----------------------------------------------------------------------------
+ * Method: void arpSendRequest(struct sr_instance* sr, struct sr_if* interfacePTR, uint32_t targetIP)
+ *
+ * Sends out an ARP request looking for the next hop's Ethernet Address
+ *---------------------------------------------------------------------------*/
+void arpSendRequest(struct sr_instance* sr, struct sr_if* interfacePTR, uint32_t targetIP){
+	
+	struct in_addr request;
+	uint8_t broadcast[ETHER_ADDR_LEN];      
+	
+	
+    uint8_t* requestPacket = malloc(42 * sizeof(uint8_t));
+    if (requestPacket == NULL) {
+        fprintf(stderr, "Error: malloc failed to make a new request packet\n");
+        return;
+    }
+	
+	memset(requestPacket, 0, 42 * sizeof(uint8_t));
+	
+	struct sr_ethernet_hdr* ethernetHdr = (struct sr_ethernet_hdr*) requestPacket;
+	struct sr_arphdr* arpHeader = (struct sr_arphdr*)(requestPacket+14);
+	
+	int i=0;
+	for(i = 0; i<ETHER_ADDR_LEN; i++){
+		broadcast[i] = 0xff;
+	}
+	
+	makeArp(arpHeader, htons(ARPHDR_ETHER), htons(ETHERTYPE_IP), 6, 4, htons(ARP_REQUEST), iface->addr, iface->ip,
+            broadcast, tip);
+			
+	makeEthernet(ethernetHdr, ETHERTYPE_ARP, interfacePTR->addr, broadcast);
+	
+	// Send the request
+	sr_send_packet(sr, requestPacket, sizeof(requestPacket), iface->name);
+	
+	request.s_addr = targetIP;
+	printf("<- ARP Request: who has %s?\n", inet_ntoa(requested));
+}
+
+/*-----------------------------------------------------------------------------
  * Method: void makeEthernet( struct sr_ethernet_hdr* ethernetHdr, uint16_t type, uint8_t* src, uint8_t* dst)
  *
  * modifies an Ethernet packet with the parameters passed
@@ -315,3 +432,53 @@ void makeEthernet(
         ethernetHdr->ether_dhost[i] = dbuf[i];
     }
 }
+
+
+/*-----------------------------------------------------------------------------
+ * Method: void isBroadcast(struct sr_ethernet_hdr* eHdr)
+ *
+ * determines if this ethernet packet is a broadcast packet or not
+ *---------------------------------------------------------------------------*/
+int isBroadcast(struct sr_ethernet_hdr* eHdr){
+	
+	int i=0;
+	for(i=0; i<ETHER_ADDR_LEN; i++){
+		if(eHdr->ether_dhost != 0xff){
+			return 0;  // This is not a broadcast packet
+		}
+	}
+	
+	return 1; // This is a broadcast packet
+}
+
+/*-----------------------------------------------------------------------------
+ * Method: void packetForUS(struct sr_instance* sr, uint8_t* packet, const char* iface)
+ *
+ * determines if this packet was meant for us, or if we have to forward this packet
+ *---------------------------------------------------------------------------*/
+int packetForUs(struct sr_instance* sr, uint8_t* packet, const char* iface){
+	
+	struct sr_ethernet_hdr* eHdr = (struct sr_ethernet_hdr*)packet;
+    struct sr_if* incoming_if = sr_get_interface(sr, interface);
+
+	// ARP packet
+    if (ntohs(eHdr->ether_type) == ETHERTYPE_ARP) {
+        struct sr_arphdr* arpHdr = (struct sr_arphdr*)(packet+14);
+        if (incoming_if->ip == arpHdr->ar_tip)
+            return 1;
+    } 
+	
+	// IP packet
+	else if (ntohs(eHdr->ether_type) == ETHERTYPE_IP) {
+        struct ip* ipHdr = (struct ip*)(packet+14);
+        if (incoming_if->ip == ipHdr->ip_dst.s_addr)
+            return 1;
+    }
+
+    return 0;
+}
+
+
+
+
+
